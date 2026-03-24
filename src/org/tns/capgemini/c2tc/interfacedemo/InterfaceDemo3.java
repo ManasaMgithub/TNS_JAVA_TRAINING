6261df5f6f3882233e92956918fad8ef117f7c45
Manasa M <savithac439@gmail.com>
2026-03-24 23:07:39 +0530

    initial commit

--- src/org/tns/capgemini/c2tc/interfacedemo/InterfaceDemo3.java
+++ src/org/tns/capgemini/c2tc/interfacedemo/InterfaceDemo3.java
@@ -1,48 +1,10 @@
 package org.tns.capgemini.c2tc.interfacedemo;
+import java.lang.FunctionalInterface;
 
-interface Vehicle7
-{
-	void start();
+@FunctionalInterface
+public interface InterfaceDemo3 {
 	
-	void stop();
-}
+	public abstract void sayHello(String Name); //functional method
 
-interface Electrivehicle extends  Vehicle7
-{
-	void chargeBattery();
-}
-
-class Tesla implements Electrivehicle
-{
-
-	@Override
-	public void start() {
-		System.out.println("vehicle is starting");
-		
-	}
-
-	@Override
-	public void stop() {
-		System.out.println("vehicle is stopping");
-		
-	}
-
-	@Override
-	public void chargeBattery() {
-		System.out.println("Battery is charged");
-		
-	}
-	
-}
-public class Interface_imp_Interface {
-
-	public static void main(String[] args) {
-		Tesla tesla=new Tesla();
-		tesla.start();
-		tesla.stop();
-		tesla.chargeBattery();
-		
-
-	}
 
 }
--- src/org/tnsif/cap/c2tc/managerscode/KIA_Rules.java
+++
@@ -1,9 +0,0 @@
-package org.tnsif.cap.c2tc.managerscode;
-
-public class KIA_Rules { 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
